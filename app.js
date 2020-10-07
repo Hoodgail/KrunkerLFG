901dde71b93b2466aabfc963ea95289d131745d3
Hoodgail <45743294+Hoodgail@users.noreply.github.com>
2020-10-07 09:35:19 -0400

    Update app.js
    Now it looks like an actual JS code and not just a 3 year old writing a summary

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 // Load dependencies
-const Discord = require('discord.js');
-const config = require("./config.json");
-const client = new Discord.Client({
-    fetchAllMembers: false,
-
-});
-const fs = require("fs");
-const path = require("path");
-const env = require("dotenv");
-const db = require("./mongo.js");
+const Discord = require('discord.js'),
+      config = require("./config.json"),
+      client = new Discord.Client({ fetchAllMembers: false }),
+      fs = require("fs"),
+      path = require("path"),
+      env = require("dotenv"),
+      db = require("./mongo.js");
 
 //Boot up the database
-async function init() {
+(async function init() {
     await db.init().catch(console.error);
-}
-init();
+})();
 
 //Login
 env.config();
@@ -27,29 +23,41 @@ client.on('ready', async() => {
     client.user.setActivity(`v${config.version}`, { type: "WATCHING" });
 });
 
-client.on('message', async(message) => {
-    client.setTimeout(async() => {
+client.on('message', function(message) => {
+    client.setTimeout(function() {
         if (!message.deleted) {
-            if (message.author.bot) return; // This will prevent bots from using the bot. Lovely!
+            if (message.author.bot||!message.guild) return; // This will prevent bots from using the bot and will prevent the bot from responding to DMs. Lovely!
+            
+            // Managing channels
+            switch(message.channel.id){
+                case"688434522072809500": //#looking-for-game
+                    client.commands.get('lfg').run(client, message)
+                break;
 
-            if (!message.guild) return; // This will prevent the bot from responding to DMs. Lovely!
+                case"687539638168059956": //#bunker-bot-commands
+                case"679429025445445643": //#dev
+                    if (message.content.indexOf(`${config.prefix}info`) == 0) {
+                        client.commands.get('info').run(client, message);
+                    } else if (message.content.indexOf(`${config.prefix}lfg`) == 0) {
+                        client.commands.get('lfg').run(client, message);
+                    }
+                break;
 
-            if (message.channel.id == '688434522072809500') { //#looking-for-game
-                client.commands.get('lfg').run(client, message)
-            } else if (message.channel.id == '687539638168059956' || message.channel.id == '679429025445445643') { //#bunker-bot-commands and #dev
-                if (message.content.indexOf(`${config.prefix}info`) == 0) {
-                    client.commands.get('info').run(client, message);
-                } else if (message.content.indexOf(`${config.prefix}lfg`) == 0) {
-                    client.commands.get('lfg').run(client, message);
-                }
-            } else if (message.channel.id == '710454866002313248') { //#trading-board
-                client.commands.get('trading').run(client, message);
-            } else if (message.channel.id == '604386199976673291') { //#market-chat
-                client.commands.get('market').run(client, message);
-            } else if (message.channel.id == '727526422381461514') { //#krunker-art
-                client.commands.get('art').run(client, message);
-            } else if (message.channel.id == '534664336027680768') { //#suggestions
-                client.commands.get('suggestions').run(client, message);
+                case"710454866002313248":  //#trading-board
+                    client.commands.get('trading').run(client, message);
+                break;
+
+                case"604386199976673291": //#market-chat
+                    client.commands.get('market').run(client, message);
+                break;
+
+                case"727526422381461514": //#krunker-art
+                    client.commands.get('art').run(client, message);
+                break;
+
+                case"534664336027680768": //#suggestions
+                    client.commands.get('suggestions').run(client, message);
+                break;
             }
         }
     }, 250);
@@ -60,17 +68,11 @@ client.commands = new Discord.Collection(); //Discord's Collection Class, extend
 client.aliases = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    let jsfile = files.filter(f => f.split(".").pop() === "js");
-    if (jsfile.length <= 0) {
-        return console.log("[KB Bot] There aren't any commands!"); //JJ has fucked up
-    }
+    if (err) return console.error(err);
+    const js_file = files.filter(f=>f.endsWith(".js"));
+    if (js_file.length <= 0) return console.log("[KB Bot] There aren't any commands!"); //JJ has fucked up
     jsfile.forEach((f, i) => {
         const pull = require(`./commands/${f}`)
-
         client.commands.set(pull.config.name, pull);
         pull.config.aliases.forEach(alias => {
             client.aliases.set(alias, pull.config.name); //Store this data in our client(Only in cache, this data doesn't goto discord)
